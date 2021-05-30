2bec5b1c520467a997f996858bb2baf11f9823d7
Deniz Jasarbasic <Denizj2828@gmail.com>
2021-05-29 23:12:36 -0400

    initial commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var country = this.props.data.address.country;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+
+         <div className="row section-head">
+
+            <div className="two columns header-col">
+
+               <h1><span>Get In Touch.</span></h1>
+
+            </div>
+
+            <div className="ten columns">
+
+                  <p className="lead">{message}</p>
+
+            </div>
+
+         </div>
+
+         <div className="row">
+            <div className="eight columns">
+
+               <form action="" method="post" id="contactForm" name="contactForm">
+					<fieldset>
+
+                  <div>
+						   <label htmlFor="contactName">Name <span className="required">*</span></label>
+						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+                  </div>
+
+                  <div>
+						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+                  </div>
+
+                  <div>
+						   <label htmlFor="contactSubject">Subject</label>
+						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+                  </div>
+
+                  <div>
+                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                     <textarea cols="50" rows="13" id="contactMessage" name="contactMessage"></textarea>
+                  </div>
+
+                  <div>
+                     <button className="submit">Submit</button>
+                     <span id="image-loader">
+                        <img alt="" src="images/loader.gif" />
+                     </span>
+                  </div>
+					</fieldset>
+				   </form>
+
+           <div id="message-warning"> Error boy</div>
+				   <div id="message-success">
+                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+				   </div>
+           </div>
+
+
+            <aside className="four columns footer-widgets">
+               <div className="widget widget_contact">
+
+					   <h4>Further Contact Information</h4>
+					   <p className="address">
+						   {name}<br />
+						   {email} <br />
+						   {city}, {country} {zip}<br />
+						   <span>{phone}</span>
+					   </p>
+				   </div>
+
+               <div className="widget widget_tweets">
+                  <h4 className="widget-title">Latest Gitub Project</h4>
+                  <ul id="twitter">
+                     <li>
+                        <span>
+                        My team and I conducted and published a four-month group research project and study. 
+                        We used many large data sets (Big Data) to show the correlations of how education funding 
+                        systems can affect the success of students. From there we presented our findings and proposed 
+                        new solutions to combat this issue of inequality. At the end of the study, we had our abstraction 
+                        published in the 2020-2021 STEM Fellowship Scientific Journal.
+                        <a href="#"> https://github.com/Deniz-Jasa/Big-Data-Challenge-2020-2021</a>
+                        </span>
+                        <b><a href="#">20 Days Ago</a></b>
+                     </li>
+                  </ul>
+		         </div>
+            </aside>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Contact;
